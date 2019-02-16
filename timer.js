967c22a6290828b423bf0bff9f2b5642dd29a311
Janessa Tran <tranjanessa@gmail.com>
2019-02-16 16:15:35 -0600

    added countdown timer

--- timer.js
+++ timer.js
@@ -1,6 +1,31 @@
-var pomdoro_time = 25; 
-var current_time = Date.parse(new Date());
-var stop_time = new Date(current_time + pomdoro_time*60*1000);
+var intervalCount;
+var minutesDisplay = document.getElementById('minutes');
+console.log(minutesDisplay);
+var secondsDisplay = document.getElementById('seconds');
+var pomdoro_duration = 25*60;
+function startTimer(duration) {
+    console.log('timer activated!')
+    var timer = duration, minutes, seconds;
+    // update the timer display every second (1000)
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        // add padding in case minutes or seconds < 10
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10? "0" + seconds : seconds;
+        minutesDisplay.textContent = minutes;
+        secondsDisplay.textContent = seconds;
+        console.log(minutesDisplay.textContent);
+
+        // decrease timer by one second
+        if(timer-- < 0){
+            alert('Time for a break!');
+            intervalCount ++;
+            timer = duration;
+        }
+    }, 1000);
+}
 
 
 function play() {
@@ -18,3 +43,5 @@ function stop() {
 function reset() {
     
 }
+// to test that timer works
+window.onload = function() { startTimer(pomdoro_duration) };
